122a14e7daf3a069e5a5a1c6569ca8b265ce01fc
ssolunna <ssolunna29@gmail.com>
2023-03-22 15:37:16 -0500

    Complete my_each method tasks

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -6,6 +6,10 @@ end
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
+
 class Array
-  # Define my_each here
+ def my_each
+   self.length.times { |element| yield self[element] }
+   self
+ end 
 end
